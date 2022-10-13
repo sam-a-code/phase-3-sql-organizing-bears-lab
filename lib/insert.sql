0e6444173b8dfed860b758a84ff05c5246ed5389
Sam Avery <sam.avery1@gmail.com>
2022-10-13 16:43:09 -0500

    commit tree 355daf35d3488c77c62cf5d7c81306788915de9b

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 1, "F", "brown", "grumpy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 1, "F", "brown", "grumpy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 1, "F", "brown", "grumpy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 1, "F", "brown", "grumpy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 1, "M", "brown", "grumpy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 1, "F", "brown", "grumpy", 0);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 1, "M", "brown", "grumpy", 0);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 1, "F", "brown", "grumpy", 1);
+
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "F", "brown", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "F", "brown", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "brown", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "F", "brown", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "M", "brown", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "F", "brown", "grumpy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "M", "brown", "grumpy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "F", "brown", "grumpy", 1);
